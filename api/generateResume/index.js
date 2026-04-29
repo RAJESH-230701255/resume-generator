b9c0c095586790d8d8b2fa481faebca7125bdcdc
rajesh-230701255 <230701255@rajalakshmi.edu.in>
2026-04-29 14:12:44 +0530

    updated index.js code

--- api/generateResume/index.js
+++ api/generateResume/index.js
@@ -1,10 +1,22 @@
 module.exports = async function (context, req) {
-    const { name } = req.body || {};
+    try {
+        const body = req.body || {};
+        const name = body.name || "User";
 
-    context.res = {
-        status: 200,
-        body: {
-            message: "Resume generated for " + name
-        }
-    };
+        context.res = {
+            status: 200,
+            body: {
+                message: "Resume generated for " + name
+            }
+        };
+
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: {
+                message: "Server error",
+                error: error.message
+            }
+        };
+    }
 };
